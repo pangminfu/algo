70121c306e8bbf63eb6a30a789c81720d80e8ca9
mfpang <mfpang@sph.com.sg>
2020-03-12 22:34:55 +0800

    merge sort

--- sorting/merge_sort.go
+++ sorting/merge_sort.go
@@ -1,5 +1,38 @@
 package sorting
 
 func MergeSort(arr []int) []int {
-	return nil
+	return mergeSort(arr)
+}
+
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+
+	return merge(mergeSort(arr[:mid]), mergeSort(arr[mid:]))
+}
+
+func merge(left, right []int) []int {
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+
+	return slice
 }
